0b3acbbefd94d535ae93bbf2a8d2c9986086ec16
Yashlohade21 <yashlohade125@gmail.com>
2022-04-03 20:44:51 +0530

    Updates

---
+++ precache-manifest.8f6c458b6db859fcb5693c599fa46a9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b7b47cd058b417bc97079784cfda32",
+    "url": "/birthday-reminder/index.html"
+  },
+  {
+    "revision": "ce676be0829a3252d187",
+    "url": "/birthday-reminder/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birthday-reminder/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce676be0829a3252d187",
+    "url": "/birthday-reminder/static/js/main.76e73836.chunk.js"
+  },
+  {
+    "revision": "5567b67584bb31e15f4d",
+    "url": "/birthday-reminder/static/js/runtime-main.35ffc63d.js"
+  }
+]);
